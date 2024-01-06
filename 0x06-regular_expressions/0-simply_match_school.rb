3335c97600f4c17487b78dd3e11e6fe290b04d3e
ELHILALICODE <133546427+ELHILALICODE@users.noreply.github.com>
2024-01-06 13:27:09 +0100

    Update 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,23 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Please provide an argument."
-  exit 1
-end
-
-# Define the regular expression to match "School" at the end of the string
-pattern = /School$/
-
-# Get the argument from the command line
-input_string = ARGV[0]
-
-# Match the regular expression against the input string
-match_result = pattern.match(input_string)
-
-# Print the result
-if match_result
-  puts "Match: #{match_result[0]}"
-else
-  puts "No match."
-end
+puts ARGV[0].scan(/Holberton/).join
