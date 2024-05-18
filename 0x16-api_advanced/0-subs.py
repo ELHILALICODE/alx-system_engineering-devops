13e3e3fb98fdc15230ac1c0b18e5b37173b152df
ELHILALICODE <133546427+ELHILALICODE@users.noreply.github.com>
2024-05-18 16:39:45 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of total subscribers for a given
-subreddit.
-"""
 import requests
-
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of total subscribers for a
-    given subreddit.
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit. If an invalid subreddit is given, returns 0.
     """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
-    response = requests.get(url, headers=headers)
-    if (not response.ok):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-    subscriber_count = response.json().get('data').get('subscribers')
-    return subscriber_count
