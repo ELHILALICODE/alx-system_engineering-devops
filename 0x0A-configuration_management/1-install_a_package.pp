e06f4426ded10b7679224c7d24e5b1bb279b29a1
ELHILALICODE <133546427+ELHILALICODE@users.noreply.github.com>
2024-06-28 22:06:02 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# install flask from pip3
-package {'flask':
+# install_flask.pp
+
+# Ensure the pip3 package is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Use the pip provider to install Flask version 2.1.0
+pip { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
