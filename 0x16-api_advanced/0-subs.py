c8e95de231c2b14723edfd90659ddf89937c74ff
ELHILALICODE <133546427+ELHILALICODE@users.noreply.github.com>
2024-08-18 12:55:38 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,7 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    
-    url = f'http://www.reddit.com/r/{subreddit}/about.json'
+        
     headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
     
     try:
